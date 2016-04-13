b7238f24de56e869c19437f7ddf840865b8b98c9
alexf <alexf@DESKTOP-QF89MOA>
2016-04-13 15:38:05 -0300

    Add project files.

---
+++ Phoneword/Phoneword.Windows/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Phoneword.Windows
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new Phoneword.App());
+        }
+    }
+}
